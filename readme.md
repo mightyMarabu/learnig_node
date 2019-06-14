cac331b864dcca81f2593389f85f1138f11df5bb
mightyMarabu <sebastian.n.schmidt@gmail.com>
2019-06-14 09:39:50 +0200

    getting started with ol and node

---
+++ readme.md
@@ -0,0 +1,6 @@
+# getting started with openlayers and node js
+```
+sudo apt install nodejs
+npm install ol
+npm install --save-dev parcel-bundler
+```
--- third.js
+++ third.js
@@ -1,6 +1,7 @@
 //file source
 var http = require('http');
 var fs = require('fs');
+var ol = require('/node_modules/ol')
 
 http.createServer(function(req,res){
     fs.readFile('test.html', function(err, data){
